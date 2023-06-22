358b96976d0e946fc92c14c8f6e50a47a4c47f41
onexample <onexample@gmail.com>
2023-06-23 01:31:00 +0300

    chore: bootstrap project

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [hello, setHello] = useState<string>("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.text())
+      .then(setHello);
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div>{hello}</div>
+    </>
+  );
+}
+
+export default App;
